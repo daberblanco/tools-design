b4253ce6c096f32ab1714e12159f0d6ac8733bf9
Daber Blanco <daber.blanco@ulatina.net>
2024-04-02 03:14:49 -0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/banner';
+import Hero from './components/hero';
+import Dropzone from './components/dropzone';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Hero />
+      <Dropzone />
+      <Footer />
     </div>
   );
 }
